019d6acac6bf5dbc3f96b641faa6ee729aa8c55a
Vitor Arruda <arruda.gif@gmail.com>
2023-01-24 17:00:04 -0300

    Atualizacoes das aulas

---
+++ Aulas/aula33.py
@@ -0,0 +1,4 @@
+'''
+https://docs.python.org/pt-br/3/library/stdtypes.html
+Imutaveis que vimos: str, int, float, bool.
+'''
---
+++ Aulas/aula34.py
@@ -0,0 +1,13 @@
+'''
+Repeticoes
+while(enquanto)
+executa uma acao enquanto uma condicao for verdadeira
+'''
+
+condicao = True
+
+while True:
+    nome = input('Qual o seu nome: ')
+    print(f'Seu nome e {nome}')
+
+    break
